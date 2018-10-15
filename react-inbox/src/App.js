a6b8697dced289507b5dc8c2d06bf0f9b4f73db3
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-15 14:42:44 -0600

    added functionality to mark as read or unread by clicking the checkboxes

--- react-inbox/src/App.js
+++ react-inbox/src/App.js
@@ -7,24 +7,85 @@ import MessageList from './MessageList';
 class App extends Component {
 
   state = {
-    messages: []
+    messages: [],
+    isChecked: []
+
   }
 
   async componentDidMount() {
     const response = await fetch('http://localhost:8082/api/messages')
     const messages = await response.json()
     console.table(messages)
-    console.log(messages)
-    this.setState({messages: messages})
+    const checkBox = messages.map((item, i) =>{
+      let obj = {}
+      obj[item.id] = item.read
+      return obj
+    })
+    this.setState({
+      messages: messages,
+      isChecked: checkBox
+    })
   }
     
-  
+  markAsRead = async (e) => {
+    console.log(e.target.value)
+    var data = {
+      messageIds: [e.target.value],
+      command: 'read',
+      read: false
+    }
+    const response = await fetch('http://localhost:8082/api/messages', {
+      method: 'PATCH',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    })
+    const patched = await response.json()
+    console.log('returned from post', patched)
+    this.setState({
+      messages: patched
+    })
+  }
+
+  markAsUnread = async (e) => {
+    console.log(e.target.value)
+    var data = {
+      messageIds: [e.target.value],
+      command: 'read',
+      read: true
+    }
+    const response = await fetch('http://localhost:8082/api/messages', {
+      method: 'PATCH',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    })
+    const patched = await response.json()
+    console.log('returned from post', patched)
+    this.setState({
+      messages: patched
+    })
+  }
+
+  checkReadStatus = (e) =>{
+    console.log(e.target.value)
+  }
 
   render() {
     return (
       <div className="App">
         <ToolBar />
-        <MessageList messages={this.state.messages}/>
+        <MessageList 
+          messages={this.state.messages}
+          checkReadStatus={this.checkReadStatus}
+          isChecked={this.state.isChecked}
+          markAsRead={this.markAsRead}
+          markAsUnread={this.markAsUnread}
+        />
       </div>
     );
   }
--- react-inbox/src/MessageList.js
+++ react-inbox/src/MessageList.js
@@ -1,27 +1,31 @@
 import React from 'react';
 
-const MessageList = ({messages}) => {
+const MessageList = ({messages, checkReadStatus, markAsRead, markAsUnread}) => {
+    
     const loadMessages = (list) => {
         return list.map((message,i) => {
+            let isRead = message.read ? 'unread':'read'
+            let isSelected = message.selected ? 'selected':''
+            let isStared = message.starred ? '-o':''
+            let isChecked = message.selected ? message.selected: false
             return (
-        <div className="row message unread">
-            <div className="col-xs-1">
-                <div className="row">
-                    <div className="col-xs-2">
-                        <input type="checkbox" />
+                <div key={i} className={`row message ${isRead} ${isSelected}`}>
+                    <div className="col-xs-1">
+                        <div className="row">
+                            <div className="col-xs-2">
+                                <input name="checkbox" type="checkbox" checked={isChecked} onChange={message.read ? markAsRead : markAsUnread} value={message.id}/>
+                            </div>
+                            <div className="col-xs-2">
+                                <i className={`star fa fa-star${isStared}`}></i>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-xs-2">
-                        <i className="star fa fa-star"></i>
+                    <div className="col-xs-11">
+                        <a href="#">
+                            {message.subject}
+                        </a>
                     </div>
                 </div>
-            </div>
-            <div className="col-xs-11">
-                <a href="#">
-                    {message.subject}
-                </a>
-            </div>
-        </div>
-
             )
         })
     }
