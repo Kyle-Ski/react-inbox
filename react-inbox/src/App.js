a03d15c93627399226b965c7f97b77890f0b7066
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-15 10:37:14 -0600

    added HTML to MessageList and ToolBar

--- react-inbox/src/App.js
+++ react-inbox/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import ToolBar from './ToolBar';
+import ToolBar from './Toolbar';
+import MessageList from './MessageList';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <ToolBar />
+        <MessageList />
       </div>
     );
   }
